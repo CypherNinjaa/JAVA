a2d99e3ffa22dc9b957fd053fec1321cf19638b0
vikash <vikashkelly@gmail.com>
2024-12-28 23:11:01 +0530

    Hello world

--- Java/src/javabasics.java
+++ Java/src/javabasics.java
@@ -1,6 +1,8 @@
 public class javabasics {
     public static void main(String[] args) {
-        
+        System.out.print("Hello Vikash!\nHello world!\n");    // ln-> line ka space
+        System.out.print("Hello Vikash!\n");
+        System.out.print("Hello Vikash!\n");
     }
 }
 
