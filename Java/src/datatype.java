7a3d86e4480412f7d1fd232f1588fe0ff9631c2a
vikash <vikashkelly@gmail.com>
2024-12-29 18:04:58 +0530

    datatype

---
+++ Java/src/datatype.java
@@ -0,0 +1,35 @@
+public class datatype {
+    public static void main(String[] args) {
+        // byte
+        byte b = 8;
+        System.out.println("Byte value: " + b);
+
+        // char
+        char ch = 'a';
+        System.out.println("Char value: " + ch);
+
+        // boolean
+        boolean var = true;
+        System.out.println("Boolean value: " + var);
+
+        // float
+        float price = 10.4f; // 'f' is required for float literals
+        System.out.println("Float value: " + price);
+
+        // int
+        int n = 24;
+        System.out.println("Int value: " + n);
+
+        // long
+        long largeNumber = 123456789L; // 'L' is optional but recommended for readability
+        System.out.println("Long value: " + largeNumber);
+
+        // double
+        double preciseValue = 3.141592653589793;
+        System.out.println("Double value: " + preciseValue);
+
+        // short
+        short number = 240;
+        System.out.println("Short value: " + number);
+    }
+}
