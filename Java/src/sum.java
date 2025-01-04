fb5cd40a83ca847ac8d114fda4d4b9272d033450
vikash <vikashkelly@gmail.com>
2025-01-04 12:04:09 +0530

    SUM

---
+++ Java/src/sum.java
@@ -0,0 +1,9 @@
+public class sum {
+    public static void main(String[] args) {
+        int a, b, sum;
+        a = 10;
+        b = 4;
+        sum = a+b;
+        System.out.println("Sum : "+sum);
+    }
+}
