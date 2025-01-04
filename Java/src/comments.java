6ae7e4ba63b2e61940fe8e2355b569d21ef87257
vikash <vikashkelly@gmail.com>
2025-01-04 12:08:49 +0530

    comment

---
+++ Java/src/comments.java
@@ -0,0 +1,13 @@
+public class comments {
+    public static void main(String[] args) {
+        // this is comment
+        int a, b, sum; // variable
+        a = 10; // declaration
+        b = 4; // declaration
+        // multi line comment
+        /*
+         * sum = a+b;
+         * System.out.println("sum : "+sum);
+         */
+    }
+}
